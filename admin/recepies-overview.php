c8aae7bdb9bc8e8a38aa5cf19fc8e7092081c442
Jan Herzan <jan.herzan@gmail.com>
2018-01-21 14:43:49 +0100

    Recepies overview added.

--- admin/recepies-overview.php
+++ admin/recepies-overview.php
@@ -1,6 +1,7 @@
 <?php
 
 $materialList = WWT_MaterialEntity::get_materials();
+$recepiesList = WWT_ConsumptionEntity::get_consumptions();
 
 echo "<table>";
 echo "<tr>";
@@ -17,6 +18,27 @@ foreach ($materialList as $material) {
     echo "<td>" . $material->unit . "</td>";
     echo "<td>" . $material->volume . "</td>";
     echo "<td>" . $material->notes . "</td>";
+
+    echo "</tr>";
+}
+
+echo "</table>";
+
+echo "<table>";
+echo "<tr>";
+echo "<th>" . __('Product name', 'woocommerce-warehouse-transactions') . "</th>";
+echo "<th>" . __('Material name', 'woocommerce-warehouse-transactions') . "</th>";
+echo "<th>" . __('Needed volume', 'woocommerce-warehouse-transactions') . "</th>";
+echo "<th>" . __('Note', 'woocommerce-warehouse-transactions') . "</th>";
+echo "</tr>";
+
+foreach ($recepiesList as $recepie) {
+    echo "<tr>";
+
+    echo "<td>" . $recepie->productId . "</td>";
+    echo "<td>" . $recepie->materialName . "</td>";
+    echo "<td>" . $recepie->volume . "</td>";
+    echo "<td>" . $recepie->notes . "</td>";
 
     echo "</tr>";
 }
