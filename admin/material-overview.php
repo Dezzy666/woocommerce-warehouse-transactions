d80dfb2ea9a4e18c2bfb60f6c5646743f0caa5ac
Jan Herzan <jan.herzan@gmail.com>
2018-01-21 15:08:26 +0100

    Material page styled.

--- admin/material-overview.php
+++ admin/material-overview.php
@@ -1,24 +1,53 @@
+<h1><?php _e('Warehouse material overview', 'woocommerce-warehouse-transactions'); ?></h1>
+
 <?php
 
 $materialList = WWT_MaterialEntity::get_materials();
 
-echo "<table>";
-echo "<tr>";
-echo "<th>" . __('Material name', 'woocommerce-warehouse-transactions') . "</th>";
-echo "<th>" . __('Material unit', 'woocommerce-warehouse-transactions') . "</th>";
-echo "<th>" . __('Stock volume', 'woocommerce-warehouse-transactions') . "</th>";
-echo "<th>" . __('Note', 'woocommerce-warehouse-transactions') . "</th>";
-echo "</tr>";
+?>
+<div class="insertion">
+    <table class="recent">
+        <tr>
+            <th class="material-name"><?php _e('Material name', 'woocommerce-warehouse-transactions'); ?></th>
+            <th class="material-unit"><?php _e('Material unit', 'woocommerce-warehouse-transactions'); ?></th>
+            <th class="stock-volume"><?php _e('Stock volume', 'woocommerce-warehouse-transactions'); ?></th>
+            <th class="note"><?php _e('Note', 'woocommerce-warehouse-transactions'); ?></th>
+        </tr>
 
-foreach ($materialList as $material) {
-    echo "<tr>";
+    <?php
+        foreach ($materialList as $material) {
+            ?>
+            <tr>
+                <td class="material-name"><?php echo $material->name; ?></td>
+                <td class="material-unit"><?php echo $material->unit; ?></td>
+                <td class="stock-volume"><?php echo $material->volume; ?></td>
+                <td class="note"><?php echo $material->notes; ?></td>
+            </tr>
+            <?php
+        }
+    ?>
+    </table>
+</div>
 
-    echo "<td>" . $material->name . "</td>";
-    echo "<td>" . $material->unit . "</td>";
-    echo "<td>" . $material->volume . "</td>";
-    echo "<td>" . $material->notes . "</td>";
+<style>
+    .recent .material-name {
+        width: 150px;
+        text-align: left;
+    }
 
-    echo "</tr>";
-}
+    .recent .material-unit {
+        width: 100px;
+        text-align: left;
+    }
 
-echo "</table>";
+    .recent .note {
+        width: 450px;
+        text-align: left;
+    }
+
+    .recent .stock-volume {
+        width: 150px;
+        text-align: left;
+        border: 3px solid black;
+    }
+</style>
