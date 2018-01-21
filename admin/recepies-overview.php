efb4030c8000ad81624acad065f4e476b9ab16a2
Jan Herzan <jan.herzan@gmail.com>
2018-01-21 14:51:41 +0100

    Material and recepies pages separated.

--- admin/recepies-overview.php
+++ admin/recepies-overview.php
@@ -1,28 +1,6 @@
 <?php
 
-$materialList = WWT_MaterialEntity::get_materials();
 $recepiesList = WWT_ConsumptionEntity::get_consumptions();
-
-echo "<table>";
-echo "<tr>";
-echo "<th>" . __('Material name', 'woocommerce-warehouse-transactions') . "</th>";
-echo "<th>" . __('Material unit', 'woocommerce-warehouse-transactions') . "</th>";
-echo "<th>" . __('Stock volume', 'woocommerce-warehouse-transactions') . "</th>";
-echo "<th>" . __('Note', 'woocommerce-warehouse-transactions') . "</th>";
-echo "</tr>";
-
-foreach ($materialList as $material) {
-    echo "<tr>";
-
-    echo "<td>" . $material->name . "</td>";
-    echo "<td>" . $material->unit . "</td>";
-    echo "<td>" . $material->volume . "</td>";
-    echo "<td>" . $material->notes . "</td>";
-
-    echo "</tr>";
-}
-
-echo "</table>";
 
 echo "<table>";
 echo "<tr>";
