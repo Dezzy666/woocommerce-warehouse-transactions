ec3cd472a8bd5604dd996c392ab04cb8ba014b81
Jan Herzan <jan.herzan@gmail.com>
2018-01-21 14:43:34 +0100

    Headline added to materials.

--- admin/recepies-overview.php
+++ admin/recepies-overview.php
@@ -3,6 +3,12 @@
 $materialList = WWT_MaterialEntity::get_materials();
 
 echo "<table>";
+echo "<tr>";
+echo "<th>" . __('Material name', 'woocommerce-warehouse-transactions') . "</th>";
+echo "<th>" . __('Material unit', 'woocommerce-warehouse-transactions') . "</th>";
+echo "<th>" . __('Stock volume', 'woocommerce-warehouse-transactions') . "</th>";
+echo "<th>" . __('Note', 'woocommerce-warehouse-transactions') . "</th>";
+echo "</tr>";
 
 foreach ($materialList as $material) {
     echo "<tr>";
