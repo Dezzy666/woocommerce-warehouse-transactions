f6e8521b60508d123e54e3acad43591a22b08afb
Jan Herzan <jan.herzan@gmail.com>
2018-02-03 13:51:12 +0100

    Typo fixed.

--- admin/admin-page-templates.php
+++ admin/admin-page-templates.php
@@ -3,7 +3,7 @@
 function create_insertion_fields($prefix) {
     ?>
     <label for="<?php echo $prefix; ?>-quantity"><?php _e('Quantity', 'woocommerce-warehouse-transactions'); ?></label><input type="number" id="<?php echo $prefix; ?>-quantity" name="<?php echo $prefix; ?>-quantity">
-    <label for="<?php echo $prefix; ?>-quantity"><?php _e('Note', 'woocommerce-warehouse-transactions'); ?></label><input type="text" id="note" name="note">
+    <label for="<?php echo $prefix; ?>-note"><?php _e('Note', 'woocommerce-warehouse-transactions'); ?></label><input type="text" id="note" name="note">
     <div>
         <?php _e('+ value means added, - value means taken', 'woocommerce-warehouse-transactions'); ?>
     </div>
