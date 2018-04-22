7a1f5434e3b5e27640ac2bf539b38b8751003c74
Jan Herzan <jan.herzan@gmail.com>
2018-04-22 21:25:10 +0200

    String localized

--- admin/main-page.php
+++ admin/main-page.php
@@ -54,7 +54,7 @@ $isCeskeSluzbyUp = is_plugin_active('ceske-sluzby/ceske-sluzby.php');
 <h1><?php _e('Warehouse movement log', 'woocommerce-warehouse-transactions'); ?></h1>
 <div class="insertion">
     <h2><?php _e('Insert change', 'woocommerce-warehouse-transactions'); ?></h2>
-    <button class="find-button" id="find-button">Find</button>
+    <button class="find-button" id="find-button"><?php _e('Find', 'woocommerce-warehouse-transactions'); ?></button>
     <form method="post">
         <select class="product-select" id="product-id" name="product-id">
             <?php
